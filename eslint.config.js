3cef016f5e63172c4c49255b5f40c6fa9eb8abd0
Robot-Inventor <30.keita@gmail.com>
2025-03-20 00:46:23 +0900

    feat: add recommended rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import { config, configs as tseslintConfigs } from "typescript-eslint";
+import eslint from "@eslint/js";
 
-const eslintConfigNoJSDoc = config(...tseslintConfigs.recommendedTypeChecked, {
+const eslintConfigNoJSDoc = config(eslint.configs.recommended, ...tseslintConfigs.recommendedTypeChecked, {
     languageOptions: {
         parserOptions: {
             projectService: true
